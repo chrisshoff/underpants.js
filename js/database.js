89a6311588bbe3a29ac888d153ad17cc1b6643e3
Jim Geisler <jim.geisler@gmail.com>
2011-02-20 21:42:32 -0500

    Some db helper sample features. Feature list also pulls from the database. Issues with accessing the database more than once.

--- js/database.js
+++ js/database.js
@@ -10,16 +10,91 @@ var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NO
 
 var db = new Db('dynamic-node-server-mongo', new Server(host, port, {}), {native_parser:true});
 
-
 // Just a simple test to verify it's working.
-db.open(function(err, db) {
-    db.collection('features', function(err, collection) {
-        collection.insert({'a':1});
-        console.log('sample record added to db');
-        collection.remove(function(err, collection) {
-            console.log('sample record removed from db');
+db.test_db = function() {
+    db.open(function(err, db) {
+        db.collection('features', function(err, collection) {
+            collection.insert({'a':1});
+            console.log('sample record added to db');
+            collection.remove(function(err, collection) {
+                console.log('sample record removed from db');
+            });
         });
     });
-});
+};
+
+/**
+* Saves a feature. MmmHmm.
+**/
+db.save_feature = function(feature, callback) {
+    db.open(function(err, db) {
+        db.collection('features', function(err, collection) {
+            collection.insert(feature);
+            console.log('Feature has been persisted.');
+            db.close();
+            callback(true);
+        });
+    });
+};
+
+/**
+* Retrieves all features. Duh.
+**/
+db.get_all_features = function(callback) {
+    
+    db.open(function(err, db) {
+        db.collection('features', function(err, collection) {
+            collection.count(function(err, count) {
+                console.log("There are " + count + " records");
+                features = [];
+                collection.find(function(err, cursor) {
+                    cursor.each(function(err, item) {
+                        if (item != null) {
+                            features.push(item);
+                        } else {
+                            callback(features);     
+                        }
+                    });
+                });
+            });
+        });
+    });  
+};
+
+/**
+* Removes the features collection.
+**/
+db.clear_features = function() {
+    db.open(function(err, db) {
+        db.collection('features', function(err, collection) {
+            collection.remove(function(err, collection) {
+                console.log('Features db collection has been cleared.');
+            });
+        });
+    });
+};
+
+/**
+* Used when adding a fake feature.
+*/
+var test_feature = {
+    name : "test_feature",
+    url : "/test",
+    method : "GET",
+    code : "{ execute: function() {\nalert('did something');\n}, doc: function() {\nreturn 'this is a test';\n}}"
+};
+
+/**
+* Adds the fake feature.
+**/
+db.add_fake_feature = function() {
+    db.save_feature(test_feature, function(status) {
+        if (status) {
+            console.log("Feature was saved and we're done.");        
+        } else {
+            console.log("Feature didn't save correctly");
+        }
+    });
+};
 
 exports.db = db;
--- js/sample_features.js
+++ js/sample_features.js
@@ -1,4 +1,5 @@
 var querystring = require('querystring');
+    
 
 hello_world = { 
     execute : function(features, req, res, callback) {
@@ -19,12 +20,9 @@ add  = {
             post_data = querystring.parse(POST);
 
             new_feature["name"] = post_data.name;
-            new_feature["doc"] = post_data.doc;
             new_feature["url"] = post_data.url;
             new_feature["method"] = post_data.method;
-            new_feature["execute"] = function() {
-                eval(post_data.js);
-            }
+            new_feature["code"] = post_data.code;
             
         }).addListener('end', function () {
             res.writeHead(200, {'Content-Type' : 'text/html'});
@@ -34,28 +32,74 @@ add  = {
         });
     },
     doc : function() {
-        return "Adds a new feature to the feature list.";
+        return "<p>Adds a new feature to the feature list.</p>" +
+                "<p>The follow are required:</p>" +
+                "<ul>" +
+                    "<li>name [string] - unique feature name</li>" +
+                    "<li>url [string] - URL to call this feature</li>" +
+                    "<li>method [string] - HTTP method to use when calling this feature</li>" +
+                    "<li>code [string] - js object with execute [fn] and doc [fn] attributes</li>" +
+                "</ul>";
     }
 }
 
 fetch_all = {
     execute : function(features, req, res, callback) {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.write("<h1>Available Features:</h1>");
         feature_list = features.feature_list;
-        for (p in feature_list) {
-            for (m in feature_list[p]) {
-                link = "<a href='" + p + "'>" + p + "</a>";
-                res.write(link + " : " + features.feature_list[p][m].doc() + "<br />");
+        features.db.get_all_features(function(stored_feature_list) {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write("<h1>Available Features:</h1>");
+            for (p in feature_list) {
+                for (m in feature_list[p]) {
+                    link = "<a href='" + p + "'>" + p + "</a> (" + m + ")";
+                    res.write(link + " : " + features.feature_list[p][m].doc() + "<br />");
+                }
             }
-        }
-        res.end();
-        callback(true);
+            console.log("Feature list:");
+            console.log(stored_feature_list);
+            for (q in stored_feature_list) {
+                feature = stored_feature_list[q];
+                try {
+                    feature_code = eval(feature.code);
+                    res.write(feature.name + " : " + feature_code.doc() + "<br />");
+                } catch(e) {
+                    res.write(feature.name + " :  Could not evaluate feature.<br />");                    
+                }
+            }
+            res.end();
+            callback(true); 
+        });
     },
     doc : function() {
         return "Displays all current server features.";
     }
 };
+
+clear_db = {
+    execute : function(features, req, res, callback) {
+        features.db.clear_features();
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write("Database has been cleared.");
+        res.end();
+        callback(true);
+    },
+    doc : function() {
+        return "Removes persisted features."
+    }
+}
+
+test_db = {
+    execute : function(features, req, res, callback) {
+        features.db.add_fake_feature();
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write("New test feature added.");
+        res.end();
+        callback(true);
+    },
+    doc : function() {
+        return "Adds a fake feature."
+    }
+}
 
 hello_name = {
     execute : function(features, req, res, callback) {
@@ -80,6 +124,8 @@ exports.get_sample_feature_list = function() {
             "GET" : fetch_all,
             "POST" : add
             },
+        "/db/clear" : { "POST" : clear_db },
+        "/db/test" : { "POST" : test_db },
         "/hello" : { "GET" : hello_world },
         "/name" : { "GET" : hello_name }
     };
